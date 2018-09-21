9303ae4a6a1bb4a7522c6aa129d324414d863101
aslamann <aslaman@roc-dev.com>
2018-09-21 10:56:23 +0200

    tekst veranderd

---
+++ src/h04/Opdracht1.java
@@ -0,0 +1,18 @@
+package h04;
+import java.awt.*;
+import java.applet.Applet;
+
+public class Opdracht1 extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.white);
+        g.setColor(Color.black);
+        g.drawArc(10, 10, 100, 100);
+
+    }
+}
+
